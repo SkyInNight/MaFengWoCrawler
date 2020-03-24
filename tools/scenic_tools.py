3fa66badacd66fb1acde21e8fdd937f89a039ea1
NoBugNoLife <44198803+SkyInNight@users.noreply.github.com>
2020-03-24 22:47:23 +0800

    添加通过城市名获取对应json文件中景点url的方法。

--- tools/scenic_tools.py
+++ tools/scenic_tools.py
@@ -1,6 +1,51 @@
 # -*- coding: utf-8 -*-
+import json
 
 
 def get_scenic_info(scenic_url):
     scenic_id = scenic_url.split('/')[len(scenic_url.split('/')) - 1].split('.')[0]
     return scenic_id
+
+
+def get_scenic_url(city_name):
+    all_url_list = []
+    top_scenic_list = []
+    all_scenic_list = []
+    result_scenic_list = []
+    with open('../data/' + city_name + '.json', 'r', encoding='utf-8') as f:
+        while True:
+            line = f.readline().replace('\n', '')
+            if line is None or line == "":
+                break
+            top_scenic_list.append(json.loads(line))
+    with open('../data/all/' + city_name + '.json', 'r', encoding='utf-8') as f:
+        while True:
+            line = f.readline().replace('\n', '')
+            if line is None or line == "":
+                break
+            all_scenic_list.append(json.loads(line))
+    for top_scenic in top_scenic_list:
+        for scenic in top_scenic:
+            if 'current_url_list' in scenic:
+                for url in scenic['current_url_list']:
+                    all_url_list.append(url)
+                continue
+            result_scenic_list.append(scenic)
+    for all_scenic in all_scenic_list:
+        for scenic_list in all_scenic:
+            for scenic in scenic_list:
+                if 'current_url_list' in scenic:
+                    if len(scenic['current_url_list']) == 0:
+                        break
+                    for url in scenic['current_url_list']:
+                        if url in all_url_list:
+                            continue
+                        all_url_list.append(url)
+                    continue
+                if 'href' in scenic and scenic['href'] not in all_url_list:
+                    result_scenic_list.append(scenic)
+    return {'scenic_list': result_scenic_list, 'url_list': all_url_list}
+
+
+if __name__ == '__main__':
+    print(get_scenic_url('长沙'))
